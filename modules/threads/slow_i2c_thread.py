3d336ec6219fd995348fce800fc192b778301888
Henry Nester <henrynester@gmail.com>
2021-06-19 18:15:32 -0400

    added I2CInput/Output classes to handle I2C comms

---
+++ modules/threads/slow_i2c_thread.py
@@ -0,0 +1,30 @@
+import threading
+import time
+import typing
+from modules.drivers.valve import ValveBoard
+
+
+class SlowI2CThread(threading.Thread):
+    def __init__(self, valve_boards: typing.List[ValveBoard]):
+        self.valve_boards = valve_boards
+        self.t_fast_loop = 0
+        self.fast_loop_counter = 0
+        super().__init__(name='slow_i2c_thread')
+
+    def run(self):
+        if (time.time() - self.t_fast_loop) > 0.5:
+            self.t_fast_loop = time.time()
+            self.fast_loop()
+            self.fast_loop_counter += 1
+        if self.fast_loop_counter >= 5:
+            self.slow_loop()
+            self.fast_loop_counter = 0
+
+    def fast_loop(self):
+        for valve_board in self.valve_boards:
+            valve_board.valve_control.communicate()
+            valve_board.sensor.communicate()
+
+    def slow_loop(self):
+        for valve_board in self.valve_boards:
+            valve_board.valve_status.communicate()
